9b5f7ee29acbc7ff6d590ee01f841e5913ebb5ff
Sam Kelly <sam@durosoft.com>
2015-12-01 13:55:04 -0500

    Bugfix on initial Dfilter index generation code.

--- src/dfilter.cpp
+++ src/dfilter.cpp
@@ -18,15 +18,16 @@ namespace DFI {
     DNode *last_dequeue = NULL;
     std::queue<TNode*> q;
     int base_index = 0;
-    for(TNode *cur_tnode = root; !q.empty(); cur_tnode = q.front(), q.pop()) {
+    for(TNode *cur_tnode = root; cur_tnode == root || !q.empty(); cur_tnode = q.front(), q.pop()) {
       DNode *cur_dnode = new DNode();
       cur_dnode->base_index = base_index;
       cur_dnode->rhs_offset = 0;
       cur_dnode->tnode = cur_tnode;
+      cur_tnode->dnode = cur_dnode;
       if(((TNode *)cur_dnode->tnode)->parent == NULL) {
         cur_dnode->tnode_depth = 0;
       } else {
-        cur_dnode->tnode_depth = ((TNode *)cur_dnode->tnode)->dnode->tnode_depth + 1;
+        cur_dnode->tnode_depth = cur_tnode->parent->dnode->tnode_depth + 1;
       }
       // add cur_dnode to red-black tree here
       for(TNode *child : cur_tnode->children) {
@@ -36,6 +37,7 @@ namespace DFI {
       base_index++;
       last_dequeue = cur_dnode;
     }
+    std::cout << "done generating index" << std::endl;
   }
 
   void dfi_propagate_offset(DNode *node, int offset) {
--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 #include "dfilter.hpp"
 using namespace DFI;
+
+void tnode_connect(TNode *parent, TNode *child) {
+  parent->children.push_back(child);
+  child->parent = parent;
+}
+
 int main() {
+  std::cout << "test started" << std::endl;
+  TNode root = TNode();
+  TNode nodeA = TNode();
+  TNode nodeB = TNode();
+  TNode nodeC = TNode();
+  TNode nodeD = TNode();
+  TNode nodeE = TNode();
+  TNode nodeF = TNode();
+  TNode nodeG = TNode();
+  TNode nodeH = TNode();
+  TNode nodeI = TNode();
+  TNode nodeJ = TNode();
+  TNode nodeK = TNode();
+  TNode nodeL = TNode();
+  TNode nodeM = TNode();
+  root.parent = NULL;
+  tnode_connect(&root, &nodeA);
+  tnode_connect(&root, &nodeB);
+  tnode_connect(&root, &nodeC);
+  tnode_connect(&nodeA, &nodeD);
+  tnode_connect(&nodeB, &nodeE);
+  tnode_connect(&nodeE, &nodeF);
+  tnode_connect(&nodeE, &nodeG);
+  tnode_connect(&nodeG, &nodeH);
+  tnode_connect(&nodeH, &nodeI);
+  tnode_connect(&nodeI, &nodeJ);
+  tnode_connect(&nodeI, &nodeK);
+  tnode_connect(&nodeI, &nodeL);
+  tnode_connect(&nodeC, &nodeM);
+  std::cout << "children: " << root.children.size() << std::endl;
+
   DFilter *filter;
-  filter = new DFilter();
-  printf("test\n");
+  filter = new DFilter(&root);
+
+  free(filter);
 }
