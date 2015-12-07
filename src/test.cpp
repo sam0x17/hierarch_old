f75f156305c0879c866eb4757831b3a9758f852a
Sam Kelly <sam@durosoft.com>
2015-12-07 05:27:53 -0500

    Test random tree generation.

--- src/test.cpp
+++ src/test.cpp
@@ -9,6 +9,7 @@
 #include <sstream>
 #include <unordered_map>
 #include <random>
+#include <algorithm>
 std::random_device rd;
 
 #define ASIZE(a) (sizeof(a) / sizeof((a)[0]))
@@ -39,6 +40,7 @@ TNode *random_node(int num_types) {
 
 TNode *generate_random_tree(int n, int branch_dist[], int dist_size, int num_types) {
   TNode *root = random_node(num_types);
+  if(n == 1) return root;
   std::queue<TNode*> q;
   q.push(root);
   int total_nodes = 1;
@@ -97,13 +99,48 @@ void tnode_to_dot(TNode *root, std::string path, std::string type_names[], int n
   file.close();
 }
 
+void test_generate_random_tree() {
+  std::cout << std::endl;
+  std::cout << "testing random tree generation..." << std::endl;
+  int branch_dist[] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 4, 5};
+  int num_nodes = 1;
+  for(int i = 0; i < 7; i++) {
+    TNode *root = generate_random_tree(num_nodes, branch_dist, ASIZE(branch_dist), 50);
+    std::cout << num_nodes << " nodes... " << std::flush;
+    std::queue<TNode*> q;
+    assert(root->parent == NULL);
+    q.push(root);
+    while(!q.empty()) {
+      TNode *node = q.front();
+      q.pop();
+      if(node->parent != NULL) {
+        // node's parent contains node
+        assert(std::find(node->parent->children.begin(),
+                         node->parent->children.end(),
+                         node) != node->parent->children.end());
+      }
+      for(TNode *child : node->children) {
+        assert(node != child);
+        assert(child != root);
+        assert(child->parent == node);
+        q.push(child);
+      }
+    }
+    TNode::delete_tree(root);
+    assert(root == NULL);
+    std::cout << "[OK]" << std::endl;
+    num_nodes *= 10;
+  }
+}
+
 int main() {
   std::cout << "test suite started" << std::endl;
-  std::cout << "generating random tree" << std::endl;
+  /*std::cout << "generating random tree" << std::endl;
   std::string type_names[] = {"A", "B", "C", "D", "E", "F", "G"};
   int branch_dist[] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 4, 5};
   TNode *rootA = generate_random_tree(200, branch_dist, ASIZE(branch_dist), ASIZE(type_names));
   tnode_to_dot(rootA, "bin/output.dot", type_names, ASIZE(type_names));
   std::cout << "done" << std::endl;
-  TNode::delete_tree(rootA);
+  TNode::delete_tree(rootA);*/
+  test_generate_random_tree();
 }
