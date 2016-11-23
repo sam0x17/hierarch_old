64ba96ac4586cea72f2ef92718d4d08db7144dba
Sam Kelly <sam@durosoft.com>
2016-11-23 01:14:34 -0500

    basic capybara/poltergeist setup

--- empirical_study/lib/empirical_study.rb
+++ empirical_study/lib/empirical_study.rb
@@ -1,5 +1,20 @@
-require "empirical_study/version"
+require 'empirical_study/version'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: false)
+end
+
+Capybara.default_driver = :poltergeist
+Capybara.run_server = false
+
 
 module EmpiricalStudy
-  # Your code goes here...
+  def self.start
+    session = Capybara::Session.new :poltergeist
+    session.visit 'https://github.com'
+    session.save_and_open_screenshot
+  end
 end
