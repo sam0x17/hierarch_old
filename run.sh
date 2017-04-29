9b630acb226df21f745c911089be9549ab09cc4f
Sam Kelly <sam@durosoft.com>
2017-04-29 11:53:11 -0400

    rm -rf: the best project improvement strategy since 1991 ;)

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./build.sh
-./bin/hierarch
-#dot -Tpng bin/type_avl_tree.dot -o type_avl_tree.png
-#dot -Tpng bin/avl_tree.dot -o avl_tree.png
-#dot -Tpng bin/tnode_tree.dot -o tnode_tree.png
