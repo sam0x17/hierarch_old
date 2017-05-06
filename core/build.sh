f6bf1668ff38cec77cc0516b90a6f9bec360b657
Sam Kelly <sam@durosoft.com>
2017-05-06 01:39:04 -0400

    use opt directory for 3rd party tools, update build script

--- core/build.sh
+++ core/build.sh
@@ -2,17 +2,23 @@
 mkdir -p include || exit 1
 rm include/* -rf || exit 1
 mkdir -p bin || exit 1
+mkdir -p opt || exit 1
 rm -rf bin/* || exit 1
-cd bin || exit 1
-git clone git@github.com:greensky00/avltree.git || exit 1
-git clone git@github.com:greg7mdp/sparsepp.git || exit 1
-mkdir -p ../include/sparsepp || exit 1
-mkdir -p ../include/hierarch || exit 1
-mkdir -p ../include/avltree || exit 1
-cp sparsepp/sparsepp/*.h ../include/sparsepp/ || exit 1
-cp avltree/avltree/* ../include/avltree || exit 1
-sed -i 's/avl_parent/_avl_parent/g' ../include/avltree/* || exit 1
-cd .. || exit 1
-rm bin/* -rf || exit 1
+if [ ! -d "opt/sparsepp" ]; then
+  cd opt || exit 1
+  git clone git@github.com:greg7mdp/sparsepp.git || exit 1
+  cd .. || exit 1
+fi
+if [ ! -d "opt/avltree" ]; then
+  cd opt || exit 1
+  git clone git@github.com:greensky00/avltree.git || exit 1
+  cd .. || exit 1
+fi
+mkdir -p include/sparsepp || exit 1
+mkdir -p include/hierarch || exit 1
+mkdir -p include/avltree || exit 1
+cp opt/sparsepp/sparsepp/*.h include/sparsepp/ || exit 1
+cp opt/avltree/avltree/* include/avltree || exit 1
+sed -i 's/avl_parent/_avl_parent/g' include/avltree/* || exit 1
 cp src/*.h include/hierarch/ || exit 1
 g++ src/*.cpp include/avltree/avltree.c -std=c++14 -static -Wall -O3 -I include -o bin/hierarch || exit 1
