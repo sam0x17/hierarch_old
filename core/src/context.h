9937b18c405c33671ad70097b75e2baa44fee3e2
Sam Kelly <sam@durosoft.com>
2017-05-07 13:53:17 -0400

    adjust context class hierarchy

--- core/src/context.h
+++ core/src/context.h
@@ -4,20 +4,24 @@
 #include <hierarch/hierarch.h>
 
 namespace Hierarch {
-  class ContextInfo {
+  class ContextBase {
   public:
-    context_id_t context_id;
-    count_t num_nodes = 0;
-    index_t min_index = 0;
-    index_t max_index = 0;
     struct avl_tree atree;
   };
 
-  class Context : public ContextInfo {
+  class TypeContext : public ContextBase {
+    TypeNode *root;
+    count_t num_nodes = 0;
+    index_t min_index = 0;
+    index_t max_index = 0;
+  };
+
+  class Context : public ContextBase {
   public:
+    context_id_t context_id;
     Node *root = NULL;
     spp::sparse_hash_map<node_id_t, Node> nodes;
-    spp::sparse_hash_map<type_id_t, ContextInfo> types;
+    spp::sparse_hash_map<type_id_t, TypeContext> types;
   };
 }
 #endif
